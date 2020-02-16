2c4a0e31dcd11459a0e6f0e27077be0d1b7f3b27
Nick Pappas <nickdpap@gmail.com>
2020-02-16 17:48:56 -0500

    Users can like/dislike songs (only ones they did not request)

--- SongRequest.java
+++ SongRequest.java
@@ -4,14 +4,60 @@ public class SongRequest {
     String name;
     String artist;
     String genre;
+    boolean playing;
     int votes;
+    int clientIp;
 
-    public SongRequest(String id, String album, String name, String artist, String genre) {
+    public SongRequest(String id, String album, String name, String artist, String genre, int clientIp) {
         this.id = id;
         this.album = album;
         this.name = name;
         this.artist = artist;
         this.genre = genre;
         this.votes = 0;
+        this.clientIp = clientIp;
+        this.playing = false;
+    }
+
+    public void likeSong(int clientIp) {
+        if (clientIp != getClientIp() && !playing) {
+            votes++;
+        }
+        //updateQueue()
+    }
+
+    public void dislikeSong(int clientIp) {
+        if (clientIp != getClientIp() && !playing) {
+            votes--;
+        }
+        //updateQueue()
+    }
+    
+    String getId() {
+        return this.id;
+    }
+
+    String getAlbum() {
+        return this.album;
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    String getArtist() {
+        return this.artist;
+    }
+
+    String getGenre() {
+        return this.genre;
+    }
+
+    int getVotes() {
+        return this.votes;
+    }
+
+    int getClientIp() {
+        return this.clientIp;
     }
 }
