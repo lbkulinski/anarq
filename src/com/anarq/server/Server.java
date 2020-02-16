2bfdff39dbaa84eb0fb239aeeccbc9324bc1ad47
Logan Kulinski <lbk@purdue.edu>
2020-02-16 18:24:35 -0500

    Created initial server package and class

---
+++ src/com/anarq/server/Server.java
@@ -0,0 +1,11 @@
+package com.anarq.server;
+
+/**
+ * A server used to handle requests made to the AnarQ application.
+ *
+ * @author Logan Kulinski, lbk@purdue.edu
+ * @version February 16, 2020
+ */
+public final class Server {
+
+}
