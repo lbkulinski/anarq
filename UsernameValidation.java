b44e64e9bd6ed7dc148501b2ed40d34ae78b769c
SidMad <siddharth.madapati@gmail.com>
2020-02-16 20:41:07 -0500

    Optimised UsernameValidation
    -Added function descriptions and comments
    -Optimised algorithm and handled an edge case where the 'bad word' was at the end of the username

--- UsernameValidation.java
+++ UsernameValidation.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.Scanner;
 
 public class UsernameValidation {
 
@@ -9,25 +8,25 @@ public class UsernameValidation {
         username = name;
     }
 
-    public boolean hasBadWords () throws IOException{
+    /**
+     * Function to read all the words from the BadWords.txt file and check if the inputted
+     * Username contains any of the words in it
+     * @return True if there is a 'bad word' in the username
+     * @throws IOException
+     */
 
+    public boolean hasBadWords () throws IOException{
 
         BufferedReader br = null;
         try {
-//            File f = new File("./BadWords.txt");
-            File f = new File("/Users/sidmad/Desktop/Projects Folder/Purdue/Spring 2020/CS 307/src/BadWords.txt");
+            File f = new File("./BadWords.txt");
+//            File f = new File("/Users/sidmad/Desktop/Projects Folder/Purdue/Spring 2020/CS 307/src/BadWords.txt");
             br = new BufferedReader(new FileReader(f));
         } catch (FileNotFoundException f) {
             System.out.println("\n\n FILE NOT FOUND \n\n");
         }
-
-        String word = null;
-
+        String word;
         while ((word = br.readLine()) != null) {
-
-//            if (username.contains(word)) {
-//                return false;
-//            }
             if (contains(word)) {
                 return true;
             }
@@ -35,35 +34,67 @@ public class UsernameValidation {
         return false;
     }
 
+    /**
+     * Function to iterate through the database to check if the inputted username has already been taken
+     * @return True if the username is already take and False if it isn't
+     */
+
+    public boolean isTaken () {
+
+        // TODO method to check if the inputted username exists in the database
+
+        return false;
+    }
+
+    /**
+     * Driver function to check if the username meets our requirements
+     * @return True if it meets our requirements and false if it doesn't
+     * @throws Exception
+     */
+
+    public boolean validateUsername() throws Exception{
+
+
+        if ((username.length() < 6) || (username.length() > 16)) {
+            return false;
+        }
+        if (isTaken()) {
+            return false;
+        }
+        if (hasBadWords()) {
+            return false;
+        }
+
+     return true;
+    }
+
+    /**
+     * Helper function to check if the inputted Username contains the specified word
+     * @param word is the word that user cannot have in the username
+     * @return true if a user has any word from the BadWords.txt in their username
+     */
 
     public boolean contains (String word) {
 
         String name = username.toLowerCase();
-        do {
 
+        do {
             int position = name.indexOf(word.charAt(0));
             if (position == -1) {
-                break;
+                return false;
             }
             try {
                 String substring = name.substring(position, (position + word.length()));
-                System.out.println("word = " + word + " substring = " + substring);
                 if (substring.equalsIgnoreCase(word)) {
-                    System.out.println("Bad word present in " + name + " is " + word);
+//                    System.out.println("Bad word present in " + username + " is " + word);
                     return true;
                 } else {
                     name = name.substring(position + 1);
                 }
-
-            } catch (StringIndexOutOfBoundsException s) {
-                return false;
+            } catch (StringIndexOutOfBoundsException s) {   // If the 'bad word' is longer than the remaining username,
+                return false;                               // It cannot be present in the username
             }
         } while (name.length() > 0);
-
-
-
-
-
 
         return false;
     }
