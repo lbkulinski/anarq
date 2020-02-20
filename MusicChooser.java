11134d9fcbd75a66d9328b5a0e7ec94329ebbc19
Nick Pappas <nickdpap@gmail.com>
2020-02-20 14:04:54 -0500

    Created MusicChooser class

--- MusicChooser.java
+++ MusicChooser.java
@@ -1,3 +1,13 @@
+import java.util.ArrayList;
+
 public class MusicChooser {
+    ArrayList<String> validGenres;
     
+    public MusicChooser() {
+        validGenres = new ArrayList<String>();
+    }
+
+    public void addValidGenre(String genre) {
+        validGenres.add(genre);
+    }
 }
