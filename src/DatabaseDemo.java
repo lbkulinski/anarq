9111804eb58622ed9b075edb8fa4d01db48eb78e
Logan Kulinski <lbkulinski@icloud.com>
2020-02-22 01:36:04 -0500

    Implemented basic MongoDB test

---
+++ src/DatabaseDemo.java
@@ -0,0 +1,69 @@
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import java.util.UUID;
+import java.util.Map;
+import org.bson.BsonDocument;
+import org.bson.BsonString;
+
+/**
+ * A demo program used to connect to our MongoDB database and perform some operations.
+ *
+ * @author Logan Kulinski, lbk@purdue.edu
+ * @version February 22, 2020
+ */
+public final class DatabaseDemo {
+    /**
+     * Connects to our MongoDB database and performs some operations.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String uriString = "mongodb+srv://lbk:List<T>@cluster0-kwfia.mongodb.net/test?retryWrites=true&w=majority";
+        MongoClientURI uri;
+        MongoClient client;
+        String databaseName = "user-database";
+        MongoDatabase database;
+        MongoCollection<Document> jammerCollection;
+        String name;
+        String username;
+        String password;
+        Map<String, Object> entry;
+        Document insertDocument;
+        BsonString searchString;
+        BsonDocument searchDocument;
+
+        uri = new MongoClientURI(uriString);
+
+        client = new MongoClient(uri);
+
+        database = client.getDatabase(databaseName);
+
+        jammerCollection = database.getCollection("jammers");
+
+        name = UUID.randomUUID()
+                   .toString();
+
+        username = UUID.randomUUID()
+                       .toString();
+
+        password = UUID.randomUUID()
+                       .toString();
+
+        entry = Map.of("name", name, "username", username, "password", password);
+
+        insertDocument = new Document(entry);
+
+        jammerCollection.insertOne(insertDocument);
+
+        searchString = new BsonString(username);
+
+        searchDocument = new BsonDocument("username", searchString);
+
+        for (Document document : jammerCollection.find(searchDocument)) {
+            System.out.printf("Found document: %s%n", document.toJson());
+        } //end for
+    } //main
+}
