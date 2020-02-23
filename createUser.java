df3d9c8389d6e8b30fe840d55b813dd65ab00cc1
SidMad <siddharth.madapati@gmail.com>
2020-02-23 17:38:58 -0500

    Database addition
    Created a demo/stock method that adds a user to our database. Needs the database and collection names

---
+++ createUser.java
@@ -0,0 +1,67 @@
+import com.mongodb.BasicDBObject;
+import com.mongodb.BulkWriteOperation;
+import com.mongodb.BulkWriteResult;
+import com.mongodb.Cursor;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.ParallelScanOptions;
+import com.mongodb.ServerAddress;
+
+import java.net.UnknownHostException;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class createUser {
+
+    protected String firstName;
+    protected String lastName;
+
+    protected String username;
+    protected int birthDay;
+    protected int birthMonth;
+    protected int birthYear;
+
+    public createUser(String firstName, String lastName, String username, int birthDay, int birthMonth, int birthYear) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.birthDay = birthDay;
+        this.birthMonth = birthMonth;
+        this.birthYear = birthYear;
+
+    }
+
+
+    /**
+     * Method to add a user to the database
+     * @return 1 if the user was successfully added or 0 if the user was not added to the database
+     */
+    public int addUser() {
+
+        try {
+            MongoClient mongoClient = new MongoClient();
+
+            DBObject newUser = new BasicDBObject("username", username)
+                    .append("First name", firstName)
+                    .append("Last name", lastName)
+
+                    .append("DOB", new BasicDBObject("Day", birthDay)
+                            .append("Month", birthMonth)
+                            .append("Year", birthYear));
+
+
+            DB userDatabase = mongoClient.getDB("Insert name of  DB here");
+            DBCollection collection = database.getCollection("Insert name of Collections here");
+        <p > collection.insert(newUser);
+        } catch (UnknownHostException e) {
+            return 0;
+        }
+        return 1;
+    }
+}
