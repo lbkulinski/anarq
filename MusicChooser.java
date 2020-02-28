47bc7a4a736a7a68ae35800f93c32c44aa4c638b
Npappas0 <nickdpap@gmail.com>
2020-02-27 21:15:45 -0500

    Adding Nick's class files

---
+++ MusicChooser.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class MusicChooser {
+    ArrayList<String> validGenres;
+    
+    public MusicChooser() {
+        validGenres = new ArrayList<String>();
+    }
+
+    public void addValidGenre(String genre) {
+        validGenres.add(genre);
+    }
+
+    public boolean isValidGenre(String genre) {
+        return validGenres.contains(genre);
+    }
+}
