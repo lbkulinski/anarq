09d84a956ee07734f930aa14d3f208d2a7abd9ca
Logan Kulinski <lbkulinski@icloud.com>
2020-03-11 11:02:14 -0400

    Added additional error checking and updating code for updating a username

--- src/main/java/com/anarq/update/username/UpdateUsernameController.java
+++ src/main/java/com/anarq/update/username/UpdateUsernameController.java
@@ -11,6 +11,16 @@ import java.util.Objects;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.FindIterable;
+
+import java.io.BufferedReader;
+import java.util.Set;
+import java.util.HashSet;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
+import com.mongodb.client.result.UpdateResult;
+import com.mongodb.client.model.Updates;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
@@ -22,7 +32,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
  * A controller for updating a user's username.
  *
  * @author Logan Kulinski, lbk@purdue.edu
- * @version March 8, 2020
+ * @version March 11, 2020
  */
 @Controller
 public final class UpdateUsernameController {
@@ -33,19 +43,20 @@ public final class UpdateUsernameController {
      * @param username the username to be used in the operation
      * @return {@code true}, if a user with the specified username and user type is present in the database and
      * {@code false} otherwise
+     * @throws NullPointerException if the specified user type or username is {@code null}
      */
     private boolean userPresent(UserType userType, String username) {
-        final String format = "mongodb+srv://%s:%s@cluster0-kwfia.mongodb.net/test?retryWrites=true&w=majority";
+        String format = "mongodb+srv://%s:%s@cluster0-kwfia.mongodb.net/test?retryWrites=true&w=majority";
         String databaseUsername;
         String databasePassword;
         String uri;
         MongoClient client;
-        final String databaseName = "user-database";
+        String databaseName = "user-database";
         MongoDatabase userDatabase;
         String collectionName;
         MongoCollection<Document> collection;
         Bson filter;
-        final String fieldName = "username";
+        String fieldName = "username";
         FindIterable<Document> results;
         boolean present;
 
@@ -95,19 +106,20 @@ public final class UpdateUsernameController {
      * @param password the password to be used in the operation
      * @return {@code true}, if a user with the specified username entered the correct password and {@code false}
      * otherwise
+     * @throws NullPointerException if the specified user type, username, or password is {@code null}
      */
     private boolean passwordCorrect(UserType userType, String username, String password) {
-        final String format = "mongodb+srv://%s:%s@cluster0-kwfia.mongodb.net/test?retryWrites=true&w=majority";
+        String format = "mongodb+srv://%s:%s@cluster0-kwfia.mongodb.net/test?retryWrites=true&w=majority";
         String databaseUsername;
         String databasePassword;
         String uri;
         MongoClient client;
-        final String databaseName = "user-database";
+        String databaseName = "user-database";
         MongoDatabase userDatabase;
         String collectionName;
         MongoCollection<Document> collection;
         Bson filter;
-        final String fieldName = "username";
+        String fieldName = "username";
         FindIterable<Document> results;
         Document result;
         String readPasswordHash;
@@ -159,6 +171,121 @@ public final class UpdateUsernameController {
     } //correctPassword
 
     /**
+     * Returns whether or not the specified new username is valid. A username is valid if it does not contain any
+     * "bad words".
+     *
+     * @param newUsername the new username to be used in the operation
+     * @return {@code true}, if the specified new username is valid and {@code false} otherwise
+     * @throws NullPointerException if the specified new username is {@code null}
+     * @throws UncheckedIOException if an I/O error occurs while trying to read the file containing the bad words
+     */
+    private boolean newUsernameValid(String newUsername) {
+        String fileName = "src/main/BadWords.txt";
+        BufferedReader reader;
+        String line;
+        Set<String> badWords;
+        String pattern;
+
+        Objects.requireNonNull(newUsername, "the specified new username is null");
+
+        newUsername = newUsername.toLowerCase();
+
+        badWords = new HashSet<>();
+
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+
+            line = reader.readLine();
+
+            while (line != null) {
+                line = line.toLowerCase();
+
+                badWords.add(line);
+
+                line = reader.readLine();
+            } //end while
+
+            reader.close();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        } //end try catch
+
+        for (String badWord : badWords) {
+            pattern = String.format(".*%s.*", badWord);
+
+            if (newUsername.matches(pattern)) {
+                return false;
+            } //end if
+        } //end for
+
+        return true;
+    } //newUsernameValid
+
+    /**
+     * Attempts to update the username of the user with the specified current username with the specified new username.
+     *
+     * @param userType the user type to be used in the operation
+     * @param currentUsername the current username to be used in the operation
+     * @param newUsername the new username to be used in the operation
+     * @return {@code true}, if the user's username was successfully updated and {@code false} otherwise
+     * @throws NullPointerException if the specified user type, current username, or new username is {@code null}
+     */
+    private boolean updateUsername(UserType userType, String currentUsername, String newUsername) {
+        String databaseUsername;
+        String databasePassword;
+        String format = "mongodb+srv://%s:%s@cluster0-kwfia.mongodb.net/test?retryWrites=true&w=majority";
+        String uri;
+        MongoClient client;
+        String databaseName = "user-database";
+        MongoDatabase userDatabase;
+        String collectionName;
+        MongoCollection<Document> collection;
+        Bson filter;
+        String fieldName = "username";
+        Bson update;
+        UpdateResult result;
+
+        Objects.requireNonNull(userType, "the specified user type is null");
+
+        Objects.requireNonNull(currentUsername, "the specified current username is null");
+
+        Objects.requireNonNull(newUsername, "the specified new username is null");
+
+        databaseUsername = System.getProperty("database-username");
+
+        databasePassword = System.getProperty("database-password");
+
+        uri = String.format(format, databaseUsername, databasePassword);
+
+        client = MongoClients.create(uri);
+
+        userDatabase = client.getDatabase(databaseName);
+
+        switch (userType) {
+            case DJ:
+                collectionName = "djs";
+                break;
+            case JAMMER:
+                collectionName = "jammers";
+                break;
+            default:
+                throw new IllegalStateException(String.format("unexpected user type: %s", userType));
+        } //end switch
+
+        collection = userDatabase.getCollection(collectionName);
+
+        filter = Filters.eq(fieldName, currentUsername);
+
+        update = Updates.set(fieldName, newUsername);
+
+        result = collection.updateOne(filter, update);
+
+        client.close();
+
+        return result.getModifiedCount() == 1;
+    } //updateUsername
+
+    /**
      * Displays the form for updating a user's username and collects the user's input.
      *
      * @param model the model to be used in the operation
@@ -180,18 +307,28 @@ public final class UpdateUsernameController {
     @PostMapping("/updateUsername")
     public String updateUsernameSubmit(@ModelAttribute UserInformation userInformation) {
         UserType userType = userInformation.getUserType();
-        String username = userInformation.getUsername();
+        String currentUsername = userInformation.getUsername();
         String password = userInformation.getPassword();
         String newUsername = userInformation.getNewValue();
 
-        if (!userPresent(userType, username)) {
+        currentUsername = currentUsername.toLowerCase();
+
+        newUsername = newUsername.toLowerCase();
+
+        if (!this.userPresent(userType, currentUsername)) {
             return "updateUsernameNotFoundResult";
-        } else if (!passwordCorrect(userType, username, password)) {
+        } else if (!this.passwordCorrect(userType, currentUsername, password)) {
             return "updateUsernameIncorrectPasswordResult";
-        } else if (userPresent(userType, newUsername)) {
+        } else if (Objects.equals(currentUsername, newUsername)) {
+            return "updateUsernameSameResult";
+        } else if (this.userPresent(userType, newUsername)) {
             return "updateUsernameNewUsernameTakenResult";
+        } else if (!this.newUsernameValid(newUsername)) {
+            return "updateUsernameInvalidNewUsernameResult";
         } else {
-            return "updateUsernameResult";
+            boolean success = this.updateUsername(userType, currentUsername, newUsername);
+
+            return success ? "updateUsernameSuccessResult" : "updateUsernameFailureResult";
         } //end if
     } //updatePasswordSubmit
 }
