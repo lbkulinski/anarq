7ee8e4e28913225bd55a28dd464411a28669a510
Amul Chaulagain <47240250+amulchaulagain@users.noreply.github.com>
2020-04-22 06:00:39 -0400

    Created Backend functionality for music playback

--- core/src/main/java/com/anarq/spotify/ClientCredentialsExample.java
+++ core/src/main/java/com/anarq/spotify/ClientCredentialsExample.java
@@ -1,26 +1,27 @@
 package com.anarq.spotify;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+
 import com.wrapper.spotify.SpotifyApi;
 import com.wrapper.spotify.SpotifyHttpManager;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
 import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
 import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
 import com.wrapper.spotify.model_objects.miscellaneous.Device;
+import com.wrapper.spotify.model_objects.specification.Track;
 import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
 import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
 import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
+import com.wrapper.spotify.requests.data.player.*;
 import com.wrapper.spotify.requests.data.search.simplified.SearchPlaylistsRequest;
-import com.wrapper.spotify.requests.data.player.GetUsersAvailableDevicesRequest;
-import com.wrapper.spotify.requests.data.player.PauseUsersPlaybackRequest;
-import com.wrapper.spotify.requests.data.player.StartResumeUsersPlaybackRequest;
 
-import java.awt.*;
+import com.wrapper.spotify.requests.data.tracks.GetTrackRequest;
+import org.apache.hc.core5.http.ParseException;
+
 import java.io.IOException;
 import java.net.URI;
-import java.util.Scanner;
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
+
 
 public class ClientCredentialsExample {
     /**
@@ -31,9 +32,15 @@ public class ClientCredentialsExample {
      * information for an album, searching for artists/albums, etc.
      * NOTE: This does not return a refresh token
      */
+
+    class MyDevice {
+        String name;
+        String id;
+    }
+
     private static String clientId = "ad002d4ba9fa4766b6a6ad03fd440d46"; // This is the Client ID for our Spotify Application
     private static String clientSecret = "e1e7830578254628af9d7b10dca9341e"; // This is the Client Secret for our Spotify Application
-    private static URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:8888/authorized");
+    private static URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:8080/connect.html");
     private static String code = "";
 
     public static final SpotifyApi spotifyApi = new SpotifyApi.Builder() // Creating the Spotify API object spotifyApi
@@ -49,43 +56,36 @@ public class ClientCredentialsExample {
 //          .show_dialog(true)
             .build();
 
-    private static GetUsersAvailableDevicesRequest getUsersAvailableDevicesRequest = null;
-    private static PauseUsersPlaybackRequest pauseUsersPlaybackRequest = null;
-    private static StartResumeUsersPlaybackRequest startResumeUsersPlaybackRequest = null;
     private static AuthorizationCodeRequest authorizationCodeRequest;
 
 
 
     public static void authorizationCode_Sync() {
-        try {
-            final AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
-
-            // Set access and refresh token for further "spotifyApi" object usage
-            spotifyApi.setAccessToken(authorizationCodeCredentials.getAccessToken());
-            spotifyApi.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
-
-            System.out.println("Expires in: " + authorizationCodeCredentials.getExpiresIn());
-        } catch (IOException | SpotifyWebApiException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
+        spotifyApi.setAccessToken(getToken());
     }
 
+    public static void reauthorize(String token) {
+        spotifyApi.setAccessToken(token);
+    }
 
-    public static void authorizationCodeUri_Sync() {
+    public static String authorizationCodeUri_Sync() {
         final URI uri = authorizationCodeUriRequest.execute();
-        Desktop desktop = java.awt.Desktop.getDesktop();
-        try {
-            desktop.browse(uri);
-        } catch(IOException e) {
-            System.out.println("IO EXCEPTION");
-        }
-        Scanner input = new Scanner(System.in);
-        String line = input.nextLine();
-        code = line;
-        authorizationCodeRequest = spotifyApi.authorizationCode(code)
-                .build();
+        return uri.toString();
     }
 
+    public static void setAuthorizationCodeRequest(String code) {
+        authorizationCodeRequest = spotifyApi.authorizationCode(code).build();
+    }
+
+    public static String getToken() {
+        try {
+            AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
+            return authorizationCodeCredentials.getAccessToken();
+        } catch (IOException | SpotifyWebApiException | ParseException gt) {
+            System.out.println("Error: " + gt.getMessage());
+        }
+        return null;
+    }
 
     public static void clientCredentials_Sync() {
         /*
@@ -97,75 +97,96 @@ public class ClientCredentialsExample {
 
         try {
             final ClientCredentials clientCredentials = clientCredentialsRequest.execute(); // executes the request for the client credentials object
-
-
             spotifyApi.setAccessToken(clientCredentials.getAccessToken()); // Set access token for further "spotifyApi" object usage
             System.out.println("Expires in: " + clientCredentials.getExpiresIn()); // prints how much time before this access token will expire
         } catch (IOException | SpotifyWebApiException e) {
-            System.out.println("RIP");
             System.out.println("Error: " + e.getMessage()); // catches any errors and prints exceptions
             return;
         }
     }
 
-    public static void clientCredentials_Async() {
-        /*
-         *This method executes the client credential request asynchronously, creating a temporary access
-         * token to use Spotify features. This is an asynchronous function, so it will allow multiple
-         * requests to be executed simultaneously. Therefore, requests will not have to wait for one
-         * another to finish execution before starting its own.
-         */
-
+    public static MyDevice[] getDevices() {
+        GetUsersAvailableDevicesRequest getUsersAvailableDevicesRequest = spotifyApi.getUsersAvailableDevices().build();
         try {
-            final CompletableFuture<ClientCredentials> clientCredentialsFuture = clientCredentialsRequest.executeAsync();
+            Device[] devices = getUsersAvailableDevicesRequest.execute();
+            if(devices.length == 0) {
+                return null;
+            }
+            MyDevice[] deviceList = new MyDevice[devices.length];
+            String[] deviceNames = new String[devices.length];
+            for(int i = 0; i < devices.length; i++) {
+                deviceList[i].name = devices[i].getName();
+                deviceList[i].id = devices[i].getId();
+            }
+            return deviceList;
+        } catch (IOException | SpotifyWebApiException | ParseException gd) {
+            System.out.println("Error: " + gd.getMessage());
+        }
+        return null;
+    }
 
-            // Thread free to do other tasks...
-
-            // Example Only. Never block in production code.
-            final ClientCredentials clientCredentials = clientCredentialsFuture.join(); // unsure of what this does...
-
-            spotifyApi.setAccessToken(clientCredentials.getAccessToken()); // sets access token for further "spotifyApi" usage
-
-            System.out.println("Expires in: " + clientCredentials.getExpiresIn()); // prints how much time before this access token expires
-        } catch (CompletionException e) { //catches completion exception which does [unknown]
-            System.out.println("Error: " + e.getCause().getMessage());
-        } catch (CancellationException e) { // catches cancellation exception which does [unknown]
-            System.out.println("Async operation cancelled.");
+    public static void chooseDevice(MyDevice[] devices, int i) {
+        JsonArray deviceIds = JsonParser.parseString("[\"" + devices[i].name + "\"]").getAsJsonArray();
+        TransferUsersPlaybackRequest transferUsersPlaybackRequest = spotifyApi.transferUsersPlayback(deviceIds).build();
+        try {
+            transferUsersPlaybackRequest.execute();
+        } catch (IOException | SpotifyWebApiException | ParseException cd) {
+            System.out.println("Error: " + cd.getMessage());
         }
     }
 
-    public static void playback() {
-        getUsersAvailableDevicesRequest = spotifyApi.getUsersAvailableDevices().build();
+    public static String getSong(String id) {
+        GetTrackRequest getTrackRequest = spotifyApi.getTrack(id).build();
         try {
-            Device[] devices = getUsersAvailableDevicesRequest.execute();
-            for(int i = 0; i < devices.length; i++) {
-                Device temp = devices[i];
-                System.out.println("The name of device " + (i + 1) + " is: " + temp.getName());
-            }
-            pause();
-            Thread.sleep(5000);
-            resume();
-
-        } catch (IOException | SpotifyWebApiException | InterruptedException pb) {
-            System.out.println("Error in Playback: " + pb.getMessage());
+            Track track = getTrackRequest.execute();
+            return track.getUri();
+        } catch (IOException | SpotifyWebApiException | ParseException gs) {
+            System.out.println("Error: " + gs.getMessage());
         }
+        return null;
+    }
+
+    public static void playSongAsNext(String songID) {
+        addSong(songID);
+        skip();
+    }
+
+    public static void skip() {
+        SkipUsersPlaybackToNextTrackRequest skipUsersPlaybackToNextTrackRequest = spotifyApi.skipUsersPlaybackToNextTrack().build();
+        try {
+            skipUsersPlaybackToNextTrackRequest.execute();
+        } catch(IOException | SpotifyWebApiException | ParseException s) {
+            System.out.println("Error: " + s.getMessage());
+        }
+    }
+
+    public static void addSong(String songID) {
+        AddItemToUsersPlaybackQueueRequest addItemToUsersPlaybackQueueRequest = spotifyApi.addItemToUsersPlaybackQueue(getSong(songID)).build();
+        try {
+            String val = addItemToUsersPlaybackQueueRequest.execute();
+            System.out.println("Null: " + val);
+        } catch(IOException | SpotifyWebApiException | ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
     }
 
     public static void pause() {
-        pauseUsersPlaybackRequest = spotifyApi.pauseUsersPlayback().build();
+        PauseUsersPlaybackRequest pauseUsersPlaybackRequest = spotifyApi.pauseUsersPlayback().build();
         try {
             String returnValue = pauseUsersPlaybackRequest.execute();
-        } catch (IOException | SpotifyWebApiException p) {
+        } catch (IOException | SpotifyWebApiException | ParseException p) {
             System.out.println("Null: " + p.getMessage());
         }
     }
 
+
     public static void resume() {
-        startResumeUsersPlaybackRequest= spotifyApi.startResumeUsersPlayback().build();
+        StartResumeUsersPlaybackRequest startResumeUsersPlaybackRequest = spotifyApi.startResumeUsersPlayback().build();
 
         try {
             startResumeUsersPlaybackRequest.execute();
-        } catch (IOException | SpotifyWebApiException r) {
+        } catch (IOException | SpotifyWebApiException | ParseException r) {
             System.out.println("There was an error while trying to resume the song");
         }
     }
