5c9b6cda06ffe3b43e83db93ab338aec7d5ef244
Logan Kulinski <lbkulinski@icloud.com>
2020-04-23 18:31:52 -0500

    Added documentation and coding style changes to CoreApplication

--- core/src/main/java/com/anarq/core/CoreApplication.java
+++ core/src/main/java/com/anarq/core/CoreApplication.java
@@ -1,69 +1,83 @@
 package com.anarq.core;
 
-import com.anarq.spotify.*;
 import com.anarq.database.*;
-
 import java.util.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * An instance of the AnarQ Application.
+ *
+ * @version April 23, 2020
+ */
 @SpringBootApplication(scanBasePackages={"com.anarq"})
 public class CoreApplication {
+    /**
+     * The active sessions of this application.
+     */
+    private static final ArrayList<Session> activeSessions = new ArrayList<>();
 
-	private static ArrayList<Session> activeSessions = new ArrayList<Session>();
+    /**
+     * Runs an instance of the AnarQ Application.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
 
-	public static void main(String[] args) {
-		
-		SpringApplication.run(CoreApplication.class, args);
-		
-		ConnectToDatabase c = new ConnectToDatabase();
+        ConnectToDatabase c = new ConnectToDatabase();
+
         c.connect();
-		
-		// Initalize Encryption
-		EncryptionDecryptionAES a = new EncryptionDecryptionAES();
-		
-	}
-	
-	/* Creates a new Session and returns the created Session's ID */
-	public static Session createNewSession() {
-		
-		Session newSession = new Session();
-		activeSessions.add(newSession);
-		
-		return newSession;
-		
-	}
-	
-	/* Ends a session based on that Session's ID */
-	public static boolean terminateSession(String sessionId) {
-		
-		for (int i = 0; i < activeSessions.size(); i++) {
-			if (activeSessions.get(i).getSessionId().equals(sessionId)) {
-				
-				activeSessions.remove(i);
-				return true;
-				
-			}
-		}
-		
-		return false;
-		
-	}
-	
-	/* Using the given Id, find a session and return the reference to it,
-		otherwise, return null */
-	public static Session getSessionForSessionId(String sessionId) {
 
-		for (int i = 0; i < activeSessions.size(); i++) {
-			if (activeSessions.get(i).getSessionId().equals(sessionId)) {
-				
-				return activeSessions.get(i);
-				
-			}
-		}
-		
-		return null;
+        EncryptionDecryptionAES a = new EncryptionDecryptionAES();
+    } //main
 
-	}		
+    /**
+     * Returns a newly created session.
+     *
+     * @return a newly created session
+     */
+    public static Session createNewSession() {
+        Session newSession = new Session();
 
+        activeSessions.add(newSession);
+
+        return newSession;
+    } //createNewSession
+
+    /**
+     * Attempts to terminate the session with the specified session ID.
+     *
+     * @param sessionId the session ID to be used in the operation
+     * @return {@code true}, if the session with the specified session ID was terminated and {@code false} otherwise
+     */
+    public static boolean terminateSession(String sessionId) {
+        for (int i = 0; i < activeSessions.size(); i++) {
+            if (activeSessions.get(i).getSessionId().equals(sessionId)) {
+                activeSessions.remove(i);
+
+                return true;
+            } //end if
+        } //end for
+
+        return false;
+    } //terminateSession
+
+    /**
+     * Returns the session that is associated with the specified session ID.
+     *
+     * @param sessionId the session ID to be used in the operation
+     * @return the session that is associated with the specified session ID
+     */
+    public static Session getSessionForSessionId(String sessionId) {
+        for (Session activeSession : activeSessions) {
+            if (activeSession.getSessionId().equals(sessionId)) {
+
+                return activeSession;
+
+            }
+        }
+
+        return null;
+    } //getSessionForSessionId
 }
