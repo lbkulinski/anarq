bf8fb3d4962bd3c58f5bc11ae61ec32e23b23ba9
Logan Kulinski <lbkulinski@icloud.com>
2020-04-24 01:31:00 -0500

    Added documentation and coding style changes to EncryptionDecryptionAES

--- core/src/main/java/com/anarq/database/EncryptionDecryptionAES.java
+++ core/src/main/java/com/anarq/database/EncryptionDecryptionAES.java
@@ -1,117 +1,119 @@
 package com.anarq.database;
 
-import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
+/**
+ * A utility class for encryption and decryption in the AnarQ Application.
+ *
+ * @version April 24, 2020
+ */
 public class EncryptionDecryptionAES {
-
+    /**
+     * The cipher of this encryption/decryption utility class.
+     */
     static Cipher cipher;
-	static SecretKey secretKey;
-	
-	public EncryptionDecryptionAES() {
-		
-		try {
-		
-			KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-			keyGenerator.init(128); // block size is 128bits
-			secretKey = keyGenerator.generateKey();
-		
-		} catch (Exception e) {
-			
-			e.printStackTrace();
-			
-		}
-		
-	}
+
+    /**
+     * The secret key of this encryption/decryption utility class.
+     */
+    static SecretKey secretKey;
+
+    /**
+     * Constructs a newly allocated {@code EncryptionDecryptionAES} object.
+     */
+    public EncryptionDecryptionAES() {
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+
+            keyGenerator.init(128);
+
+            secretKey = keyGenerator.generateKey();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } //end try catch
+    } //EncryptionDecryptionAES
 
     /*
-        Code derived from:
-            https://stackoverflow.com/questions/10303767/encrypt-and-decrypt-in-java
-            https://javapapers.com/java/java-symmetric-aes-encryption-decryption-using-jce/
+     * Code derived from:
+     * https://stackoverflow.com/questions/10303767/encrypt-and-decrypt-in-java
+     * https://javapapers.com/java/java-symmetric-aes-encryption-decryption-using-jce/
      */
-
 
     /**
-     * Function to accept a plain text, encrypt it and save the new encrypted Password
-     * along wit the key used to encrypt it
-     * @param password the text to be encrypted
-     * @throws Exception
+     * Returns the encryption of the specified text.
+     *
+     * @param text the text to be used in the operation
+     * @return the encryption of the specified text
      */
-
     public static String encrypt(String text) {
-        // TODO save the encrypted password and the key
+        try {
+            cipher = Cipher.getInstance("AES");
 
-		try {
-			
-			cipher = Cipher.getInstance("AES");
-			return encryptText(text, secretKey);
-		
-		} catch (Exception e) {
-			
-			e.printStackTrace();
-			
-		}
-		
-		return "encrypt_failed";
+            return encryptText(text, secretKey);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } //encrypt
 
-    }
-	
-	public static String decrypt(String text) {
-        // TODO save the encrypted password and the key
-
-		try {
-			
-			cipher = Cipher.getInstance("AES");
-			return decryptText(text, secretKey);
-		
-		} catch (Exception e) {
-			
-			e.printStackTrace();
-			
-		}
-		
-		return "decrypt_failed";
-
-    }
+        return "encrypt_failed";
+    } //encrypt
 
     /**
-     * Function to accept a plain text, encrypt it using the given key
-     * @param plainText The text to be encrypted
-     * @param secretKey The key used to encrypt the text
-     * @return The encrypted text
-     * @throws Exception
+     * Returns the decryption of the specified text.
+     *
+     * @param text the text to be used in the operation
+     * @return the decryption of the specified text
      */
+    public static String decrypt(String text) {
+        try {
+            cipher = Cipher.getInstance("AES");
 
-    public static String encryptText(String plainText, SecretKey secretKey)
-            throws Exception {
+            return decryptText(text, secretKey);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } //end try catch
 
+        return "decrypt_failed";
+    } //decrypt
+
+    /**
+     * Returns the encryption of the specified plain text.
+     *
+     * @param plainText the plain text to be used in the operation
+     * @param secretKey the secret key to be used in the operation
+     * @return the encryption of the specified plain text
+     * @throws Exception if an exception occurs
+     */
+    public static String encryptText(String plainText, SecretKey secretKey) throws Exception {
         byte[] plainTextByte = plainText.getBytes();
+
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+
         byte[] encryptedByte = cipher.doFinal(plainTextByte);
+
         Base64.Encoder encoder = Base64.getEncoder();
-        String encryptedText = encoder.encodeToString(encryptedByte);
-        return encryptedText;
-    }
+
+        return encoder.encodeToString(encryptedByte);
+    } //encryptText
 
     /**
-     * Function to accept an encrypted text and decrypt it using the given key
-     * @param encryptedText The text to be decrypted
-     * @param secretKey The key used to decrypt the text
-     * @return The decrypted text
-     * @throws Exception
+     * Returns the decryption of the specified encrypted text.
+     *
+     * @param encryptedText the encrypted text to be used in the operation
+     * @param secretKey the secret key to be used in the operation
+     * @return the decryption of the specified encrypted text
+     * @throws Exception if an exception occurs
      */
-
-    public static String decryptText(String encryptedText, SecretKey secretKey)
-            throws Exception {
-
+    public static String decryptText(String encryptedText, SecretKey secretKey) throws Exception {
         Base64.Decoder decoder = Base64.getDecoder();
         byte[] encryptedTextByte = decoder.decode(encryptedText);
+
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
         byte[] decryptedByte = cipher.doFinal(encryptedTextByte);
-        String decryptedText = new String(decryptedByte);
-        return decryptedText;
-    }
+
+        return new String(decryptedByte);
+    } //decryptText
 }
