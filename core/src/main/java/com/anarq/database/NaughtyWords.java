ee1303e07813bc2adc7fa457012d1d6f028b9b4c
Logan Kulinski <lbkulinski@icloud.com>
2020-04-24 01:57:51 -0500

    Added documentation and coding style changes to NaughtyWords and PasswordValidation

--- core/src/main/java/com/anarq/database/NaughtyWords.java
+++ core/src/main/java/com/anarq/database/NaughtyWords.java
@@ -1,57 +1,65 @@
 package com.anarq.database;
 
-import java.io.*;
-
+/**
+ * A utility class containing the naughty words of the AnarQ Application.
+ *
+ * @version April 24, 2020
+ */
 public class NaughtyWords {
+    /**
+     * The naughty words.
+     */
+    public static final String[] naughtyWords = new String[] {
+            "anal",
+            "anus",
+            "ass",
+            "bastard",
+            "bitch",
+            "blowjob",
+            "boob",
+            "cock",
+            "cunt",
+            "cum",
+            "coon",
+            "pillock",
+            "dick",
+            "dildo",
+            "fag",
+            "fuck",
+            "homo",
+            "nig",
+            "nigger",
+            "nigga",
+            "penis",
+            "piss",
+            "pussy",
+            "queer",
+            "scrotum",
+            "sex",
+            "shit",
+            "slut",
+            "spunk",
+            "tit",
+            "twat",
+            "vagina",
+            "wank",
+            "whore",
+            "bum"
+    };
 
-	public static final String[] naughtyWords = new String[]{
-		"anal",
-		"anus",
-		"ass",
-		"bastard",
-		"bitch",
-		"blowjob",
-		"boob",
-		"cock",
-		"cunt",
-		"cum",
-		"coon",
-		"pillock",
-		"dick",
-		"dildo",
-		"fag",
-		"fuck",
-		"homo",
-		"nig",
-		"nigger",
-		"nigga",
-		"penis",
-		"piss",
-		"pussy",
-		"queer",
-		"scrotum",
-		"sex",
-		"shit",
-		"slut",
-		"spunk",
-		"tit",
-		"twat",
-		"vagina",
-		"wank",
-		"whore",
-		"bum"
-	};
+    /**
+     * Determines whether or not the specified word is naughty.
+     *
+     * @param word the word to be used in the operation
+     * @return {@code true}, if the specified word is naughty and {@code false} otherwise
+     */
+    public static boolean isANaughtyWord(String word) {
+        for (String naughtyWord : naughtyWords) {
+            if (word.contains(naughtyWord)) {
+                return true;
+            } //end if
+        } //end for
 
-	public static boolean isANaughtyWord(String word) {
-	   
-		for (int i = 0; i < naughtyWords.length; i++) {
-			if (word.contains(naughtyWords[i])) {
-				return true;
-			}
-		}
-		return false;
-	   
-	}
-
+        return false;
+    } //isANaughtyWord
 }
-
--- core/src/main/java/com/anarq/database/PasswordValidation.java
+++ core/src/main/java/com/anarq/database/PasswordValidation.java
@@ -1,111 +1,135 @@
 package com.anarq.database;
 
+/**
+ * A utility class for performing password validation in the AnarQ Application.
+ *
+ * @version April 24, 2020
+ */
 public class PasswordValidation {
-
-	private static final int MIN_PASSWORD_LENGTH = 8; 
-	private static final int MAX_PASSWORD_LENGTH = 64;
-    private final static String salt = "ANARQ";
-    private String password;
-
-    public PasswordValidation (String pswrd) {
-        password = pswrd;
-    }
+    /**
+     * The minimum length of a password.
+     */
+	private static final int MIN_PASSWORD_LENGTH = 8;
 
     /**
-     * Driver function to check if the password meets our requirements
-     * @return True if it meets our requirements and false if it doesn't
+     * The maximum length of a password.
      */
+	private static final int MAX_PASSWORD_LENGTH = 64;
 
+    /**
+     * The password of this password validation utility class.
+     */
+    private final String password;
+
+    /**
+     * Constructs a newly allocated {@code PasswordValidation} object with the specified password.
+     *
+     * @param password the password to be used in construction
+     */
+    public PasswordValidation(String password) {
+        this.password = password;
+    } //PasswordValidation
+
+    /**
+     * Returns the result of validation of the password of this password validation utility class.
+     *
+     * @return the result of validation of the password of this password validation utility class
+     */
     public String validatePassword(){
-
-
-        if ((password.length() < MIN_PASSWORD_LENGTH)) {
+        if (password.length() < MIN_PASSWORD_LENGTH) {
 			return "Password must be at least " + MIN_PASSWORD_LENGTH + " characters in length!";
-		}
-		if ((password.length() > MAX_PASSWORD_LENGTH)) {
+		} //end if
+
+		if (password.length() > MAX_PASSWORD_LENGTH) {
             return "Password must be less than " + MAX_PASSWORD_LENGTH + " characters in length!";
-        }
+        } //end if
+
         if (!containsNumbers()) {
             return "Password must contain at least one (1) numeral!";
-        }
+        } //end if
+
         if (!containsLowerCaseCharacters()) {
             return "Password must contain at least one (1) lower case character!";
-        }
+        } //end if
+
         if (!containsUpperCaseCharacters()) {
             return "Password must contain at least one (1) upper case character!";
-        }
+        } //end if
+
         if (!containsSpecialCharacters()) {
             return "Password must contain at least one (1) special character! (@,$,#,&)";
-        }
+        } //end if
+
         return "Password Ok";
-    }
+    } //validatePassword
 
     /**
-     * Helper function to check if the password contains any numbers
-     * @return True if the password contains a digit and false if it doesn't
+     * Determines whether or not the password of this password validation utility class contains numbers.
+     *
+     * @return {@code true}, if the password of this password validation utility class contains numbers and
+     * {@code false} otherwise
      */
-
     public boolean containsNumbers() {
-
         for (int i = 0; i < password.length(); i++) {
             if (Character.isDigit(password.charAt(i))) {
                 return true;
-            }
-        }
+            } //end if
+        } //end if
+
         return false;
-    }
+    } //containsNumbers
 
     /**
-     * Helper function to check if the password contains any special characters
-     * @return True if the password contains a special character and false if it doesn't
+     * Determines whether or not the password of this password validation utility class contains numbers.
+     *
+     * @return {@code true}, if the password of this password validation utility class contains numbers and
+     * {@code false} otherwise
      */
-
     public boolean containsSpecialCharacters() {
+        if (password.contains("@")) {
+            return true;
+        } //end if
 
-            if (password.contains("@")) {
-                return true;
-            }
-            if (password.contains("$")) {
-                return true;
-            }
-            if (password.contains("#")) {
-                return true;
-            }
-            if (password.contains("&")) {
-                return true;
-            }
-        return false;
+        if (password.contains("$")) {
+            return true;
+        } //end if
 
-    }
+        if (password.contains("#")) {
+            return true;
+        } //end if
+
+        return password.contains("&");
+    } //containsSpecialCharacters
 
     /**
-     * Helper function to check if the password contains any upper case Letters
-     * @return True if the password contains a upper case character and false if it doesn't
+     * Determines whether or not the password of this password validation utility class contains uppercase letters.
+     *
+     * @return {@code true}, if the password of this password validation utility class contains uppercase letters and
+     * {@code false} otherwise
      */
-
     public boolean containsUpperCaseCharacters() {
-
         for (int i = 0; i < password.length(); i++) {
             if (Character.isUpperCase(password.charAt(i))) {
                 return true;
-            }
-        }
+            } //end if
+        } //end for
+
         return false;
-    }
+    } //containsUpperCaseCharacters
 
     /**
-     * Helper function to check if the password contains any upper case Letters
-     * @return True if the password contains a upper case character and false if it doesn't
+     * Determines whether or not the password of this password validation utility class contains lowercase letters.
+     *
+     * @return {@code true}, if the password of this password validation utility class contains lowercase letters and
+     * {@code false} otherwise
      */
-
     public boolean containsLowerCaseCharacters() {
-
         for (int i = 0; i < password.length(); i++) {
             if (Character.isLowerCase(password.charAt(i))) {
                 return true;
-            }
-        }
-        return false;
-    }
+            } //end if
+        } //end for
 
+        return false;
+    } //containsLowerCaseCharacters
 }
