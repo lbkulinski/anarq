fd3392a68c931511d8d3d813c9232a4c28afa7ac
Logan Kulinski <lbkulinski@icloud.com>
2020-04-23 20:45:37 -0500

    Added documentation and coding style changes to SearchController

--- core/src/main/java/com/anarq/core/SearchController.java
+++ core/src/main/java/com/anarq/core/SearchController.java
@@ -3,51 +3,67 @@ package com.anarq.core;
 import com.anarq.spotify.*;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * A controller for searches made to the AnarQ Application.
+ *
+ * @version April 23, 2020
+ */
 @RestController
 public class SearchController {
-	
-	@GetMapping("/search")
-	public Song[] searchForSongsWithQuery(
-	@RequestParam(value="sessionId", defaultValue="default_session_id")String sessionId,
-	@RequestParam(value="query", defaultValue="no_query")String query,
-	@RequestParam(value="userId", defaultValue="no_user_id")String userId) {
-		
-		// Attempt to obtain the given session based on the sessionId provided
-		Session session = CoreApplication.getSessionForSessionId(sessionId);
-		if (session == null) {
-			System.err.println("Error: Request for non-existant session was created!\n ID: "
-				+ sessionId);
-			return null;
-		}
-		// Check if that session has that user
-		if (!session.hasUserForId(userId)) {
-			return null;
-		}	
-		// Return a list of songs from the music chooser of the session
-		return session.getMusicChooser().searchForSongs(query);
-		
-	}
-	
-	@PutMapping("/request-song")
-	public boolean addSongRequest(
-	@RequestParam(value="sessionId", defaultValue="default_session_id")String sessionId,
-	@RequestParam(value="songId", defaultValue="no_song_id")String songId,
-	@RequestParam(value="userId", defaultValue="no_user_id")String userId) {
-		
-		// Attempt to obtain the given session based on the sessionId provided
-		Session session = CoreApplication.getSessionForSessionId(sessionId);
-		if (session == null) {
-			System.err.println("Error: Request for non-existant session was created!\n ID: "
-				+ sessionId);
-			return false;
-		}
-		// Check if that session has that user
-		if (!session.hasUserForId(userId)) {
-			return false;
-		}	
-		
-		return session.requestSong(SpotifyGateway.getSongForSongId(songId), userId);
-		
-	}
-	
+    /**
+     * Returns the songs that contain the specified search query.
+     *
+     * @param sessionId the session ID to be used in the operation
+     * @param query the query to be used in the operation
+     * @param userId the user ID to be used in the operation
+     * @return the songs that contain the specified search query
+     */
+    @GetMapping("/search")
+    public Song[] searchForSongsWithQuery(
+            @RequestParam(value="sessionId", defaultValue="default_session_id") String sessionId,
+            @RequestParam(value="query", defaultValue="no_query") String query,
+            @RequestParam(value="userId", defaultValue="no_user_id") String userId) {
+        Session session = CoreApplication.getSessionForSessionId(sessionId);
+
+        if (session == null) {
+            System.err.println("Error: Request for non-existant session was created!\n ID: " + sessionId);
+
+            return null;
+        } //end if
+
+        if (!session.hasUserForId(userId)) {
+            return null;
+        } //end if
+
+        return session.getMusicChooser()
+                      .searchForSongs(query);
+    } //searchForSongsWithQuery
+
+    /**
+     * Attempts to request the song with the specified song ID in the session with the specified session ID.
+     *
+     * @param sessionId the session ID to be used in the operation
+     * @param songId the song ID to be used in the operation
+     * @param userId the user ID to be used in the operation
+     * @return {@code true}, if the song with the specified song ID was successfully requested and {@code false}
+     * otherwise
+     */
+    @PutMapping("/request-song")
+    public boolean addSongRequest(@RequestParam(value="sessionId", defaultValue="default_session_id") String sessionId,
+                                  @RequestParam(value="songId", defaultValue="no_song_id") String songId,
+                                  @RequestParam(value="userId", defaultValue="no_user_id") String userId) {
+        Session session = CoreApplication.getSessionForSessionId(sessionId);
+
+        if (session == null) {
+            System.err.println("Error: Request for non-existant session was created!\n ID: " + sessionId);
+
+            return false;
+        } //end if
+
+        if (!session.hasUserForId(userId)) {
+            return false;
+        } //end if
+
+        return session.requestSong(SpotifyGateway.getSongForSongId(songId), userId);
+    } //addSongRequest
 }
